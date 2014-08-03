e22c024766c276631b70eb9851fef715aa2c4ab4
Ron Valstar <sjeiti@gmail.com>
2014-08-03 14:45:32 +0200

    implemented search

--- src/js/totaltimeline.view.header.js
+++ src/js/totaltimeline.view.header.js
@@ -5,36 +5,45 @@ iddqd.ns('totaltimeline.view.header',(function(){
 	'use strict';
 
 	var pages = totaltimeline.pages
+		,util = totaltimeline.util
 //		,log = totaltimeline.view.log
 		,mView
 		,mNav
 		,mNavUl
 		,mSearch
+		,mSearchResult
+		,mSearchFragment = document.createDocumentFragment()
 	;
 
-	function init(model) {
+	function init() {
 		initVariables();
-		initEvents(model);
-		initView(model);
+		initEvents();
+		//initView();
 	}
 
 	function initVariables() {
 		mView = document.getElementById('pageHeader');
-		if (mView) {
+		if (mView) {// todo: refactor test
 			mNav = mView.querySelector('nav');
 			mNavUl = mNav.querySelector('ul');
-			mSearch = mView.querySelector('input');
-			console.log('mSearch',mSearch); // log
+			mSearch = document.getElementById('search');
+			mSearchResult = document.getElementById('searchResult');
 		}
 	}
 
 	function initEvents() {
-		mView&&pages.loaded.add(handlePagesLoaded);
+		if (mView) {// todo: refactor test
+			pages.loaded.add(handlePagesLoaded);
+			mSearch.addEventListener('keyup',handleKeyUp);
+			mSearch.addEventListener('focus',handleFocusBlur);
+			mSearch.addEventListener('blur',handleFocusBlur);
+		}
 	}
 
-	function initView() {
-	}
+	/*function initView() {
+	}*/
 
+	// todo: document
 	function handlePagesLoaded() {
 		mNavUl.innerHTML = ''; // todo: refactor
 		var mFragment = document.createDocumentFragment();
@@ -43,6 +52,72 @@ iddqd.ns('totaltimeline.view.header',(function(){
 			mFragment.appendChild(zen('li>a[href=#'+oPage.slug+']{'+oPage.name+'}').pop());
 		}
 		mNavUl.appendChild(mFragment);
+	}
+
+	// todo: document
+	function handleKeyUp(){
+		var sSearch = mSearch.value.toLocaleLowerCase()
+			,aFound = []
+			,collection = totaltimeline.collection
+			,pages = totaltimeline.pages
+			,i,l
+			,sText,iResult
+		;
+		l = collection.length;
+		for (i=0;i<l;i++) {
+			var aInst = collection[i];
+			for (var j=0,m=aInst.length;j<m;j++) {
+				var oItem = aInst[j];
+				sText = oItem.info.name+' '+oItem.info.explanation+' '+oItem.info.wikimedia;
+				iResult = searchString(sSearch,sText);
+				if (iResult!==0) {
+					aFound.push([iResult,oItem]);
+				}
+			}
+		}
+		l = pages.length;
+		for (i=0;i<l;i++) {
+			var oPage = pages[i];
+			sText = oPage.name+' '+oPage.copy;
+			iResult = searchString(sSearch,sText);
+			if (iResult!==0) {
+				aFound.push([iResult,oPage]);
+			}
+		}
+		util.emptyElement(mSearchFragment);
+		util.emptyElement(mSearchResult);
+		aFound.sort(function(a,b){
+			return a[0]>b[0]?-1:1;
+		});
+		l = aFound.length;
+		for (i=0;i<l;i++) {
+			var oFind = aFound[i][1]
+				,oInfo = oFind.info||oFind;
+			mSearchResult.appendChild(zen('li>a[href=#'+oInfo.slug+']{'+oInfo.name+'}').pop());
+		}
+		mSearchResult.appendChild(mSearchFragment);
+	}
+
+	// todo: document
+	function handleFocusBlur(e){
+		if (e.type==='focus') {
+			mSearchResult.style.display = 'block';
+		} else {
+			setTimeout(function(){
+				mSearchResult.style.display = 'none';
+			},400);
+		}
+	}
+
+	// todo: document
+	function searchString(needle,haystack){
+		var aHaystack = haystack.toLocaleLowerCase().split(needle)
+			,iHaystack = aHaystack.length
+			,iValue = 0;
+		if (iHaystack>1) {
+			iValue = iHaystack/(aHaystack[0].length+1);
+		}
+		return iValue;
 	}
 
 	return init;
